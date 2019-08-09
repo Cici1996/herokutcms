a2a580b9001fd54208773ee4969c19c3df4b8a5a
LK-Cipe <apesaepudin1996@gmail.com>
2019-08-09 10:27:12 +0700

    add tymon jwt

---
+++ app/Models/LpModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LpModel extends Model
+{
+    protected $table = "t_lp";
+    protected $primaryKey = "LP_Id";
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel & Heroku
+                    API TCMS with Laravel & Heroku
                 </div>
 
                 <div class="links">
