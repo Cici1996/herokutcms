c398891626d1d1f2e443a966deb728c33283eba5
LK-Cipe <apesaepudin1996@gmail.com>
2019-08-11 11:24:47 +0700

    chnge route

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware('jwt.auth')->get('users', function(Request $request) {
+Route::middleware('jwt.verify')->get('users', function(Request $request) {
     return auth()->user();
 });
 
